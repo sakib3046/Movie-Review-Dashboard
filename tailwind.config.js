f58c95a8ecdcac537719c636a7e8358d775ef735
Md. Sakib Hasan <sakib3046@gmail.com>
2024-09-05 01:49:36 +0600

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "primary":"#DAFDBA",
+        "secondary": "#9AEBA3",
+        "background":"#012030",
+        "aditional":"#45C4B0"
+      }
+    },
+  },
+  plugins: [],
+}
+
